83c2cf4b955ce4ae64ff716f59bd12845646d212
Greg Fogelberg <52933995+gfog-floqast@users.noreply.github.com>
2023-05-03 09:47:40 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 cp /workspaces/.codespaces/.persistedshare/dotfiles/.bash_aliases ~/.bash_aliases
 cp /workspaces/.codespaces/.persistedshare/dotfiles/.profile ~/.profile
+cp /workspaces/.codespaces/.persistedshare/dotfiles/.vimrc ~/.vimrc
 cp /workspaces/.codespaces/.persistedshare/dotfiles/init.vim ~/.config/nvim/init.vim
 . ~/.profile
